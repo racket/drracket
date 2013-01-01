racket/racket

commit 0f9674e1ddd598a97bec61a9e759102182a26789

adjust drracket window size and position preferences to use a different key
This will make switching back and forth between 5.1.3 and the latest
not lose the frame size and position preferences (since the format
of the data is different now that the current release remembers
the position on a per-monitor-configuration basis)

collects/drracket/private/unit.rkt
@@ -4350,8 +4350,8 @@ module browser threading seems wrong.
       (super-new
        [filename filename]
        [style '(toolbar-button)]
-       [size-preferences-key 'drracket:unit-window-size]
-       [position-preferences-key 'drracket:unit-window-position])
+       [size-preferences-key 'drracket:window-size]
+       [position-preferences-key 'drracket:window-position])
       
       (initialize-menus)
       
@@ -4571,7 +4571,7 @@ module browser threading seems wrong.
       
       (when (= 2 (length (send resizable-panel get-children)))
         (send resizable-panel set-percentages
-              (let ([p (preferences:get 'drracket:unit-window-size-percentage)])
+              (let ([p (preferences:get 'drracket:window-size-percentage)])
                 (list p (- 1 p)))))
       
       (set-label-prefix (string-constant drscheme))

collects/drracket/private/main.rkt
@@ -176,11 +176,11 @@
   (let-values ([(w h) (get-display-size)])
     (set! frame-width (min frame-width (- w window-trimming-upper-bound-width)))
     (set! frame-height (min frame-height (- h window-trimming-upper-bound-height))))
-  (frame:setup-size-pref 'drracket:unit-window-size 
+  (frame:setup-size-pref 'drracket:window-size 
                          frame-width
                          frame-height 
                          #:position-preferences
-                         'drracket:unit-window-position))
+                         'drracket:window-position))
 
 (drr:set-default 'drracket:backtrace-window-width 400 number?)
 (drr:set-default 'drracket:backtrace-window-height 300 number?)

racket/drracket

commit 68c1d9df38b8b41126232ee3086483d4b60d1b17

whoops, fix a bug introduced in 0f9674e1ddd598a97bec61a9e759102182a26789
original commit: 974350472ce94259577626d85e4e8945dd1cac27

collects/drracket/private/unit.rkt
@@ -4571,7 +4571,7 @@ module browser threading seems wrong.
       
       (when (= 2 (length (send resizable-panel get-children)))
         (send resizable-panel set-percentages
-              (let ([p (preferences:get 'drracket:window-size-percentage)])
+              (let ([p (preferences:get 'drracket:unit-window-size-percentage)])
                 (list p (- 1 p)))))
       
       (set-label-prefix (string-constant drscheme))