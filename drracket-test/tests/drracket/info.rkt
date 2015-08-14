26a556bd4b7972025bf103206992b7842d79c2d4
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2015-08-14 12:00:52 -0400

    Increase timeout.

--- drracket-test/tests/drracket/info.rkt
+++ drracket-test/tests/drracket/info.rkt
@@ -5,7 +5,8 @@
 (define compile-omit-paths '("snip" "image-and-comment-box.rkt"))
 
 (define test-timeouts '(("easter-egg.rkt" 300)
-                        ("teaching-lang-sharing-modules.rkt" 300)))
+                        ("teaching-lang-sharing-modules.rkt" 300)
+                        ("no-write-and-frame-leak.rkt" 300)))
 
 (define test-responsibles '(("test-engine-test.rkt" sperber)
                             ("teachpack.rkt" (robby matthias))
