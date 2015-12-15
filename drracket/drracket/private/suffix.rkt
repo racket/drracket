55418fa562983782025c67f035f20ebbf7d3121f
Robby Findler <robby@racket-lang.org>
2015-12-15 14:18:40 -0600

    typo

--- drracket/drracket/private/suffix.rkt
+++ drracket/drracket/private/suffix.rkt
@@ -6,7 +6,7 @@
 
 ;; all-racket-suffixes : (bytes -> string) string
 ;;  String-appends all Racket suffixes, including ".rtkd" and "rktl"
-;;  into a string wit a given separator
+;;  into a string with a given separator
 (define (all-racket-suffixes handle-one sep)
   (apply
    string-append
