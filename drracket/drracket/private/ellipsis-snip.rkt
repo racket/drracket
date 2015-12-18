46a6ce7293b0492280c5e4a3d757109f33038765
Robby Findler <robby@racket-lang.org>
2015-12-17 22:26:08 -0600

    add some mouse-over cues to ellipsis snips to make their clickability more obvious

--- drracket/drracket/private/ellipsis-snip.rkt
+++ drracket/drracket/private/ellipsis-snip.rkt
@@ -15,7 +15,9 @@
 
     (define-syntax (forward stx)
       (syntax-case stx ()
-        [(_ (def (id formals ...)))
+        [(_ (def (id formals ...))
+            (before ...)
+            (after ...))
          (with-syntax ([(call-ids ...)
                         (for/list ([formal (in-list (syntax->list #'(formals ...)))])
                           (syntax-case formal ()
@@ -23,19 +25,55 @@
                             [id (identifier? #'id) #'id]))])
            #'(def (id formals ...)
                (sync-style)
-               (send str-snip id call-ids ...)))]))
+               before ...
+               (send str-snip id call-ids ...)
+               after ...))]))
     (define/private (sync-style)
       (send str-snip set-style (get-style)))
     
-    (forward (define/override (get-extent dc x y wb hb db sb lb rb)))
-    (forward (define/override (draw dc x y left top right bottom dx dy draw-caret)))
-    (forward (define/override (partial-offset dc x y len)))
-    (forward (define/override (split position first second)))
-    (forward (define/override (size-cache-invalid)))
-    
+    (forward (define/override (get-extent dc x y wb hb db sb lb rb)) () ())
+    (forward (define/override (draw dc x y left top right bottom dx dy draw-caret))
+             {(unless insertion-done?
+                (when inside?
+                  (define pen (send dc get-pen))
+                  (define brush (send dc get-brush))
+                  (define wb (box 0.0))
+                  (define hb (box 0.0))
+                  (get-extent dc (- x dx) (- y dy) wb hb #f #f #f #f)
+                  (send dc set-brush "pink" 'solid)
+                  (send dc set-pen "black" 1 'transparent)
+                  (send dc draw-rectangle x y (unbox wb) (unbox hb))
+                  (send dc set-brush "red" 'solid)
+                  (send dc draw-rectangle
+                        x
+                        (+ y (unbox hb) -1)
+                        (unbox wb)
+                        1)
+                  (send dc set-pen pen)
+                  (send dc set-brush brush)))}
+             {})
+    (forward (define/override (partial-offset dc x y len)) () ())
+    (forward (define/override (split position first second)) () ())
+    (forward (define/override (size-cache-invalid)) () ())
+
+    (define inside? #f)
+    (define/private (update-inside _inside?)
+      (unless (equal? inside? _inside?)
+        (set! inside? _inside?)
+        (define admin (get-admin))
+        (when admin
+          (define wb (box 0.0))
+          (define hb (box 0.0))
+          (define dc (send admin get-dc))
+          (when dc
+            (get-extent dc 0 0 wb hb #f #f #f #f)
+            (send admin needs-update this 0 0 (unbox wb) (unbox hb))))))
+          
     (define/override (on-goodbye-event dc x y editorx editory event)
+      (update-inside #f)
       (handle-event dc x y editorx editory event #t))
     (define/override (on-event dc x y editorx editory event)
+      (update-inside #t)
       (handle-event dc x y editorx editory event #f))
     (define/private (handle-event dc x y editorx editory event goodbye?)
       (unless insertion-done?
@@ -51,6 +89,7 @@
           (send ed set-cursor the-cursor-to-use))))
     
     (define/private (do-insertion)
+      (update-inside #f)
       (define admin (get-admin))
       (define ed (send admin get-editor))
       (when (is-a? ed text:ports<%>)
