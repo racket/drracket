99204242e7f623a9913efb842e33f235c5d7d08d
Robby Findler <robby@racket-lang.org>
2016-01-03 09:40:08 -0600

    allow test case to pass when bug icon isn't present

--- drracket-test/tests/drracket/module-lang-test.rkt
+++ drracket-test/tests/drracket/module-lang-test.rkt
@@ -272,10 +272,10 @@
       @rx{expt: contract violation.*given: #<void>})
 (test @t{#lang racket/base}
       @t{1 2 ( 3 4}
-      @t{1@"\n"2@"\n". . read: expected a `)' to close `('})
+      #rx"1\n2\n[.][^\n]*read: expected a `[)]' to close `[(]'")
 (test @t{#lang racket/base}
       "1 2 . 3 4"
-      "1\n2\n. . read: illegal use of `.'")
+      #rx"1\n2\n[.][^\n]*read: illegal use of `[.]'")
 (test @t{#lang racket/base}
       "1 2 (lambda ()) 3 4"
       "1\n2\n. lambda: bad syntax in: (lambda ())")
