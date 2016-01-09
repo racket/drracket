eb3d5f1cf7f396c8bec9a3a97247353ce1164679
Robby Findler <robby@racket-lang.org>
2016-01-09 10:18:02 -0600

    add more tests

--- drracket-test/tests/drracket/get-defs-test.rkt
+++ drracket-test/tests/drracket/get-defs-test.rkt
@@ -15,10 +15,10 @@
 
 (define-syntax (test-definitions stx)
   (syntax-case stx ()
-    [(_ string ((name start end) ...))
+    [(_ string stuff ... ((name start end) ...))
      #`(let ([actual (map (match-lambda [(defn _ n s e)
                                          (list n s e)])
-                          (get-definitions/string string))]
+                          (get-definitions/string string stuff ...))]
              [expected (list (list name start end) ...)])
          (unless (equal? actual expected)
            (eprintf "Test failure at ~a\nActual: ~s\nExpected: ~s\n"
@@ -66,3 +66,53 @@ END
   ("h" 85 145)
   ("i" 146 193)
   ((string-constant end-of-buffer-define) 194 214)))
+
+(test-definitions
+ #<<END
+(define-metafunction L
+  f : p -> q
+  [(f p) t])
+(define-metafunction L
+  [(g p) t])
+(define-metafunction/extension f L
+  h : p -> q
+  [(h p) t])
+(define-metafunction/extension f L
+  [(i p) t])
+(define-metafunction
+END
+ #:define-prefix
+ (list (define-popup-info "(define" "(define ...)" "δ")
+       (define-popup-info "(module" "(module ...)" "M"))
+ (("f" 0 48)
+  ("g" 49 84)
+  ("h" 85 145)
+  ("i" 146 193)
+  ((string-constant end-of-buffer-define) 194 214)))
+
+(test-definitions
+ #<<END
+(module m racket/base 1)
+(module+ test 2)
+(define a 1)
+(define b 1)
+END
+ #:define-prefix
+ (list (define-popup-info "(define" "(define ...)" "δ")
+       (define-popup-info "(module" "(module ...)" "M"))
+ (("m" 0 24) ("test" 25 41) ("a" 42 54) ("b" 55 67)))
+
+(test-definitions
+ #<<END
+(module a racket/base 1)
+(define b 1)
+(module+ c 2)
+(define d 1)
+(module e 1)
+(module f 2)
+(define g 1)
+END
+ #:define-prefix
+ (list (define-popup-info "(define" "(define ...)" "δ")
+       (define-popup-info "(module" "(module ...)" "M"))
+ (("a" 0 24) ("b" 25 37) ("c" 38 51) ("d" 52 64) ("e" 65 77) ("f" 78 90) ("g" 91 103)))
