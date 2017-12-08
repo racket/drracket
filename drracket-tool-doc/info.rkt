1d502ed7e185686eb879d7934282a39416b6fb1e
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2017-12-07 23:28:45 -0500

    Fix missing dependency.
    http://drdr.racket-lang.org/43950/pkg-src/build/make#output1

--- drracket-tool-doc/info.rkt
+++ drracket-tool-doc/info.rkt
@@ -3,7 +3,7 @@
 (define collection 'multi)
 
 (define deps '("base" "scribble-lib" "drracket-tool-lib"))
-(define build-deps '("racket-doc" "gui-doc" "gui-lib"))
+(define build-deps '("racket-doc" "gui-doc" "gui-lib" "drracket"))
 
 (define pkg-desc "Docs for the programmatic interface to some IDE tools that DrRacket supports")
 
