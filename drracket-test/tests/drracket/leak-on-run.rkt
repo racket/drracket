967ed444bf5e2940c652f731ca4322fd649b29fb
Robby Findler <robby@racket-lang.org>
2018-03-13 21:26:40 -0500

    use a better strategy for the leak on run test, namely start with a baseline memory use that is the size after running a bunch of time. If things grow after, we have a leak (but if things grew after the old way of setting up the test, we don't necc have a leak)

--- drracket-test/tests/drracket/leak-on-run.rkt
+++ drracket-test/tests/drracket/leak-on-run.rkt
@@ -20,13 +20,15 @@
 
    ;; try to let various caches and whatnot
    ;; warm up to some stable size
-   (for ([i (in-range 40)])
+   (define initial-size 0)
+   (for ([i (in-range 20)])
      (do-execute drr)
-     (collect-garbage) (collect-garbage))
-       
+     (collect-garbage) (collect-garbage)
+     (set! initial-size (max initial-size (current-memory-use))))
+
    (define times-exceeded-previous-maximum
      (let loop ([i 10]
-                [previous-maximum #f])
+                [previous-maximum initial-size])
        (cond
          [(zero? i) 0]
          [else
@@ -34,16 +36,13 @@
           (collect-garbage) (collect-garbage)
           (define this-time (current-memory-use))
           (define exceeded-previous-maximum
-            (and previous-maximum
-                 (previous-maximum . < . this-time)
+            (and (previous-maximum . < . this-time)
                  (- this-time previous-maximum)))
           (printf "went up? ~s ~s\n" this-time exceeded-previous-maximum)
           (+ (if (and exceeded-previous-maximum
                       (exceeded-previous-maximum . > . 10000))
                  1 0)
-             (loop (- i 1) (if previous-maximum
-                               (max previous-maximum this-time)
-                               this-time)))])))
+             (loop (- i 1) (max previous-maximum this-time)))])))
    (when (times-exceeded-previous-maximum . > . 5)
      (eprintf "test failed, exceeded previous maximum by 10,000 ~a times\n"
               times-exceeded-previous-maximum))))
