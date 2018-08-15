racket/drracket

commit d75e4795c67672e775e4bf1b16b4fc9a5155560b

add backtrace to out-of-memory errors (when available)

drracket/drracket/private/debug.rkt
@@ -45,7 +45,7 @@
           [prefix drracket:language-configuration: drracket:language-configuration/internal^]
           [prefix drracket:init: drracket:init^]
           [prefix drracket: drracket:interface^])
-  (export drracket:debug^)
+  (export drracket:debug/int^)
   
   ;                                                          
   ;                                                          
@@ -341,13 +341,11 @@
                                            (send rep get-definitions-text)))])
     (let* ([stack1 (or pre-stack '())]
            [stack2 (if (exn? exn)
-                       (map cdr (filter cdr (cut-stack-at-checkpoint exn)))
+                       (cut-stack-at-checkpoint (exn-continuation-marks exn))
                        '())]
            [port-name-matches-cache (make-hasheq)]
-           [stack1-editions (map (λ (x) (srcloc->edition/pair defs ints x port-name-matches-cache))
-                                 stack1)]
-           [stack2-editions (map (λ (x) (srcloc->edition/pair defs ints x port-name-matches-cache))
-                                 stack2)]
+           [stack1-editions (get-editions port-name-matches-cache defs ints stack1)]
+           [stack2-editions (get-editions port-name-matches-cache defs ints stack2)]
            [src-locs (cond
                        [(exn:srclocs? exn)
                         ((exn:srclocs-accessor exn) exn)]
@@ -391,6 +389,11 @@
              (when (and ints (exn:fail:out-of-memory? exn))
                (define frame (send ints get-top-level-window))
                (send ints no-user-evaluation-dialog frame #f #t #f))))))))
+
+  ;; =User= =Kernel=
+  (define (get-editions port-name-matches-cache defs ints stack)
+    (map (λ (x) (srcloc->edition/pair defs ints x port-name-matches-cache))
+         stack))
   
   (define (render-message lines)
     (define collected (collect-hidden-lines lines))
@@ -561,18 +564,26 @@
   ;; =User=
   (define (print-bug-to-stderr msg cms1 editions1 cms2 editions2 defs ints)
     (when (port-writes-special? (current-error-port))
-      (let ([note% (if (mf-bday?) mf-note% bug-note%)])
-        (when note%
-          (let ([note (new note%)])
-            (send note set-stacks cms1 cms2)
-            (send note set-callback
-                  (λ (snp) (show-backtrace-window/edition-pairs/two msg 
-                                                                    cms1 editions1
-                                                                    cms2 editions2
-                                                                    defs ints)))
-            (write-special note (current-error-port))
-            (display #\space (current-error-port)))))))
-  
+      (define note (make-note-to-print-to-stderr msg cms1 editions2 cms2 editions2 defs ints))
+      (when note
+        (write-special note (current-error-port))
+        (display #\space (current-error-port)))))
+
+  ;; =Kernel= =User=
+  (define (make-note-to-print-to-stderr msg cms1 editions1 cms2 editions2 defs ints)
+    (define note% (if (mf-bday?) mf-note% bug-note%))
+    (cond
+      [note%
+       (define note (new note%))
+       (send note set-stacks cms1 cms2)
+       (send note set-callback
+             (λ (snp) (show-backtrace-window/edition-pairs/two msg
+                                                               cms1 editions1
+                                                               cms2 editions2
+                                                               defs ints)))
+       note]
+      [else #f]))
+
   ;; display-srclocs-in-error : (listof src-loc) -> void
   ;; prints out the src location information for src-to-display
   ;; as it would appear in an error message

drracket-plugin-lib/drracket/private/drsig.rkt
@@ -3,6 +3,7 @@
 
 (provide drracket:eval^
          drracket:debug^
+         drracket:debug/int^
          drracket:module-language^
          drracket:module-language/int^
          drracket:module-language-tools^
@@ -98,6 +99,11 @@
    ;display-srclocs-in-error
    ;show-syntax-error-context
    ))
+
+(define-signature drracket:debug/int^ extends drracket:debug^
+  (make-note-to-print-to-stderr
+   get-editions
+   cms->srclocs))
 
 (define-signature drracket:module-language-cm^
   (module-language<%>))

drracket/drracket/private/rep.rkt
@@ -33,6 +33,7 @@ TODO
          "stack-checkpoint.rkt"
          "parse-logger-args.rkt"
          "insulated-read-language.rkt"
+         "stack-checkpoint.rkt"
          
          ;; the dynamic-require below loads this module, 
          ;; so we make the dependency explicit here, even
@@ -56,7 +57,7 @@ TODO
           [prefix drracket:unit: drracket:unit^]
           [prefix drracket:text: drracket:text^]
           [prefix drracket:help-desk: drracket:help-desk^]
-          [prefix drracket:debug: drracket:debug^]
+          [prefix drracket:debug: drracket:debug/int^]
           [prefix drracket:eval: drracket:eval^]
           [prefix drracket:module-language: drracket:module-language/int^]
           [prefix drracket: drracket:interface^])
@@ -936,9 +937,45 @@ TODO
       (field (need-interaction-cleanup? #f))
       
       (define/private (no-user-evaluation-message frame exit-code memory-killed?)
+        (define-values (cms1 cms2)
+          (let ([ut (get-user-thread)])
+            (cond
+              [ut
+               (define cms (continuation-marks ut))
+               (values (drracket:debug:cms->srclocs cms)
+                       (cut-stack-at-checkpoint cms))]
+              [else (values '() '())])))
         (no-user-evaluation-dialog frame exit-code memory-killed? #t)
         (set-insertion-point (last-position))
-        (insert-warning "\nInteractions disabled"))
+        (define have-some-stack? (or (pair? cms1) (pair? cms2)))
+        (when have-some-stack?
+          (insert-out-of-memory-stacktrace cms1 cms2
+                                           (send frame get-definitions-text)
+                                           (send frame get-interactions-text)))
+        (insert-warning
+         (string-append (if have-some-stack? "" "\n")
+                        "Interactions disabled"
+                        (if memory-killed?
+                            "; out of memory"
+                            ""))))
+
+      (define/private (insert-out-of-memory-stacktrace cms1 cms2 defs ints)
+        (define locked? (is-locked?))
+        (when locked? (lock #f))
+        (begin-edit-sequence)
+        (define cache (make-hasheq))
+        (define note
+          (drracket:debug:make-note-to-print-to-stderr
+           (string-constant program-ran-out-of-memory)
+           cms1
+           (drracket:debug:get-editions cache defs ints cms1)
+           cms2
+           (drracket:debug:get-editions cache defs ints cms2)
+           defs ints))
+        (insert-before note)
+        (insert-before " ")
+        (end-edit-sequence)
+        (when locked? (lock #t)))
       
       (define/public (no-user-evaluation-dialog frame exit-code memory-killed? program-terminated?)
         (define new-limit (and custodian-limit (+ custodian-limit custodian-limit)))
@@ -1266,7 +1303,8 @@ TODO
               (thread
                (λ () ; =Kernel=
                  (let ([ut (get-user-thread)])
-                   (thread-wait ut)
+                   (sync (thread-suspend-evt ut)
+                         (thread-dead-evt ut))
                    (queue-system-callback
                     ut
                     (λ () ; =Kernel=, =Handler=
@@ -1298,7 +1336,7 @@ TODO
                                   custodian-limit
                                   user-custodian-parent))
         (let ([user-eventspace (parameterize ([current-custodian user-custodian])
-                                 (make-eventspace))])
+                                 (make-eventspace #:suspend-to-kill? #t))])
           (set! user-eventspace-box (make-weak-box user-eventspace))
           (set! user-break-parameterization (parameterize-break 
                                              #t 

drracket/drracket/private/stack-checkpoint.rkt
@@ -8,31 +8,33 @@
   (define checkpoint #f)
   (call-with-exception-handler
    (λ (exn)
-     (when (and checkpoint ; just in case there's an exception before it's set
-                (not (hash-has-key? checkpoints exn)))
-       (hash-set! checkpoints exn checkpoint))
+     (when checkpoint ; just in case there's an exception before it's set
+       (define key (if (exn? exn) (exn-continuation-marks exn) exn))
+       (unless (hash-has-key? checkpoints key)
+         (hash-set! checkpoints key checkpoint)))
      exn)
    (lambda ()
      (set! checkpoint (current-continuation-marks))
      (thunk))))
 ;; returns the stack of the input exception, cutting off any tail that was
 ;; registered as a checkpoint
-(define (cut-stack-at-checkpoint exn)
-  (define stack (continuation-mark-set->context (exn-continuation-marks exn)))
+(define (cut-stack-at-checkpoint cont-marks)
+  (define stack (continuation-mark-set->context cont-marks))
   (define checkpoint
-    (cond [(hash-ref checkpoints exn #f) => continuation-mark-set->context]
+    (cond [(hash-ref checkpoints cont-marks #f) => continuation-mark-set->context]
           [else #f]))
-  (if (not checkpoint)
-    stack
-    (let loop ([st stack]
-               [sl (length stack)]
-               [cp checkpoint]
-               [cl (length checkpoint)])
-      (cond [(sl . > . cl) (cons (car st) (loop (cdr st) (sub1 sl) cp cl))]
-            [(sl . < . cl) (loop st sl (cdr cp) (sub1 cl))]
-            [(equal? st cp) '()]
-            [else (loop st sl (cdr cp) (sub1 cl))]))))
-  
+  (define stack-with-gaps-and-extra-info
+    (if checkpoint
+        (let loop ([st stack]
+                   [sl (length stack)]
+                   [cp checkpoint]
+                   [cl (length checkpoint)])
+          (cond [(sl . > . cl) (cons (car st) (loop (cdr st) (sub1 sl) cp cl))]
+                [(sl . < . cl) (loop st sl (cdr cp) (sub1 cl))]
+                [(equal? st cp) '()]
+                [else (loop st sl (cdr cp) (sub1 cl))]))
+        stack))
+  (map cdr (filter cdr stack-with-gaps-and-extra-info)))
 
 (define-syntax-rule (with-stack-checkpoint expr)
   (call-with-stack-checkpoint (lambda () expr)))

drracket/info.rkt
@@ -21,7 +21,7 @@
                ["string-constants-lib" #:version "1.19"]
                "typed-racket-lib"
                "wxme-lib"
-               ("gui-lib" #:version "1.32")
+               ["gui-lib" #:version "1.35"]
                ("racket-index" #:version "1.2")
                "racket-doc"
                "html-lib"

commit c03aaba70e541d31d2c0c0b19e74edd12fa7e919

fix bug introduced in d75e4795c6

drracket/drracket/private/debug.rkt
@@ -564,13 +564,23 @@
   ;; =User=
   (define (print-bug-to-stderr msg cms1 editions1 cms2 editions2 defs ints)
     (when (port-writes-special? (current-error-port))
-      (define note (make-note-to-print-to-stderr msg cms1 editions2 cms2 editions2 defs ints))
+      (define note (make-note-to-print-to-stderr msg cms1 editions1 cms2 editions2 defs ints))
       (when note
         (write-special note (current-error-port))
         (display #\space (current-error-port)))))
 
   ;; =Kernel= =User=
   (define (make-note-to-print-to-stderr msg cms1 editions1 cms2 editions2 defs ints)
+    (unless (= (length cms1) (length editions1))
+      (raise-argument-error 'make-note-to-print-to-stderr
+                            "length of cms1 to match length of cm2"
+                            2
+                            msg cms1 editions1 cms2 editions2 defs ints))
+    (unless (= (length cms2) (length editions2))
+      (raise-argument-error 'make-note-to-print-to-stderr
+                            "length of cms1 to match length of cm2"
+                            2
+                            msg cms1 editions1 cms2 editions2 defs ints))
     (define note% (if (mf-bday?) mf-note% bug-note%))
     (cond
       [note%