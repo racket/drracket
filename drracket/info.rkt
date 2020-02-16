3052c870d7a84b82e798c2195b89e961a253ad1c
Alexander Shopov <ash@kambanaria.org>
2020-02-16 15:06:56 +0100

    Extract new translatable constants, bump dependency
    Signed-off-by: Alexander Shopov <ash@kambanaria.org>

--- drracket/info.rkt
+++ drracket/info.rkt
@@ -18,7 +18,7 @@
                "sandbox-lib"
                ("scribble-lib" #:version "1.11")
                ("snip-lib" #:version "1.2")
-               ["string-constants-lib" #:version "1.31"]
+               ["string-constants-lib" #:version "1.32"]
                "typed-racket-lib"
                "wxme-lib"
                ["gui-lib" #:version "1.45"]
