00a2cc28a80be5ef095c0ab757a3f48c3db55f42
Robby Findler <robby@racket-lang.org>
2020-04-05 10:49:19 -0500

    clean up test suite
    label and organize the tests; use define-get-arrows everywhere that it
    could have been used and make it generate less code Also, delete one
    test that was a copy of another test in the file.

--- drracket-tool-test/tests/check-syntax/syncheck-direct.rkt
+++ drracket-tool-test/tests/check-syntax/syncheck-direct.rkt
@@ -7,115 +7,40 @@
          syntax/modread
          racket/file)
 
-(check-true
- (let ()
-   (define add-arrow-called? #f)
-   
-   (define annotations
-     (new (class (annotations-mixin object%)
-            (super-new)
-            (define/override (syncheck:find-source-object stx)
-              (if (eq? 'the-source (syntax-source stx))
-                  'yep
-                  #f))
-            (define/override (syncheck:add-arrow . args)
-              (set! add-arrow-called? #t)))))
-   
-   (define-values (add-syntax done)
-     (make-traversal (make-base-namespace)
-                     (current-directory)))
-   
-   (parameterize ([current-annotations annotations]
-                  [current-namespace (make-base-namespace)])
-     (add-syntax (expand
-                  (read-syntax
-                   'the-source
-                   (open-input-string
-                    (format "~s"
-                            `(module m racket/base
-                               (define x 4)
-                               x
-                               (let ([y 1]) y)))))))
-     (done))
-   add-arrow-called?))
-
-(check-true
- (let ()
-   (define add-arrow-called? #f)
-   
-   (define annotations
-     (new (class (annotations-mixin object%)
-            (super-new)
-            (define/override (syncheck:find-source-object stx)
-              (if (eq? 'the-source (syntax-source stx))
-                  'yep
-                  #f))
-            (define/override (syncheck:add-arrow . args)
-              (set! add-arrow-called? #t)))))
-   
-   (define-values (add-syntax done)
-     (make-traversal (make-base-namespace) #f))
-   
-   (parameterize ([current-annotations annotations]
-                  [current-namespace (make-base-namespace)])
-     (add-syntax (expand
-                  (read-syntax
-                   'the-source
-                   (open-input-string
-                    (format "~s"
-                            `(module m racket/base
-                               (define x 4)
-                               x
-                               (let ([y 1]) y)))))))
-     (done))
-   add-arrow-called?))
-
-(check-not-exn
- (λ ()
-   (define annotations
-     (new (class (annotations-mixin object%)
-            (super-new)
-            (define/override (syncheck:find-source-object stx)
-              stx))))
-   
-   (define base-namespace (make-base-namespace))
-   (define-values (add-syntax done)
-     (make-traversal base-namespace #f))
-   
-   (parameterize ([current-annotations annotations]
-                  [current-namespace base-namespace])
-     (eval '(require (for-syntax racket/base)))
-     (add-syntax
-      (expand
-       '(let-syntax ([m (λ (_) #`(let ([x 1]) x))])
-          (m))))
-     (done))))
-
-
-(define-syntax-rule (define-get-arrows get-what-arrows method-header arrow-info)
+(define-syntax-rule
+  (define-get-arrows get-what-arrows method-header arrow-info)
   (define (get-what-arrows str)
-    (define tail-arrows '())
-    
-    (define annotations
-      (new (class (annotations-mixin object%)
-             (super-new)
-             (define/override (syncheck:find-source-object stx)
-               (if (eq? 'the-source (syntax-source stx))
-                   'yep
-                   #f))
-             (define/override method-header
-               (set! tail-arrows (cons arrow-info tail-arrows))))))
-    
-    (define-values (add-syntax done)
-      (make-traversal (make-base-namespace) #f))
-    
-    (parameterize ([current-annotations annotations]
-                   [current-namespace (make-base-namespace)])
-      (add-syntax (expand
-                   (parameterize ([read-accept-reader #t])
-                     (read-syntax 'the-source (open-input-string str)))))
-      (done))
-    (reverse tail-arrows)))
+    (define-get-arrows/proc
+      (λ (%)
+        (class %
+          (inherit add-item)
+          (define/override method-header
+            (add-item arrow-info))
+          (super-new)))
+      str)))
+
+(define (define-get-arrows/proc mixin str)
+  (define results '())
+
+  (define annotations
+    (new (mixin (class (annotations-mixin object%)
+                  (super-new)
+                  (define/public (add-item x) (set! results (cons x results)))
+                  (define/override (syncheck:find-source-object stx)
+                    (if (eq? 'the-source (syntax-source stx))
+                        'yep
+                        #f))))))
+
+  (define-values (add-syntax done)
+    (make-traversal (make-base-namespace) #f))
+
+  (parameterize ([current-annotations annotations]
+                 [current-namespace (make-base-namespace)])
+    (add-syntax (expand
+                 (parameterize ([read-accept-reader #t])
+                   (read-syntax 'the-source (open-input-string str)))))
+    (done))
+  (apply set results))
 
 ;                                                                       
 ;                                                                       
@@ -135,28 +60,28 @@
 ;                                                                       
 ;                                                                       
 
-(define-get-arrows get-tail-arrows 
+(define-get-arrows get-tail-arrows
   (syncheck:add-tail-arrow parent-src parent-pos child-src child-pos)
   (list parent-pos child-pos))
 
 (check-equal? (get-tail-arrows "#lang racket/base\n(if 1 2 3)")
-              '((18 24) (18 26)))
+              (set '(18 24) '(18 26)))
 (check-equal? (get-tail-arrows "#lang racket/base\n(λ (x) 1 2)")
-              '((18 28)))
+              (set '(18 28)))
 (check-equal? (get-tail-arrows "#lang racket/base\n(case-lambda [(x) 1 2][(y z) 3 4 5 6])")
-              '((18 38) (18 53)))
+              (set '(18 38) '(18 53)))
 (check-equal? (get-tail-arrows "#lang racket/base\n(let ([x 3]) (#%expression (begin 1 2)))")
-              '((18 45) (45 54)))
+              (set '(18 45) '(45 54)))
 (check-equal? (get-tail-arrows "#lang racket/base\n(begin0 1)")
-              '((18 26)))
+              (set '(18 26)))
 (check-equal? (get-tail-arrows "#lang racket/base\n(begin0 1 2)")
-              '())
+              (set))
 (check-equal? (get-tail-arrows "#lang racket/base\n(letrec ([x (lambda (y) x)]) (x 3))")
-              '((30 42) (18 47)))
+              (set '(30 42) '(18 47)))
 (check-equal? (get-tail-arrows "#lang racket/base\n(with-continuation-mark 1 2 3)")
-              '((18 46)))
+              (set '(18 46)))
 (check-equal? (get-tail-arrows "#lang racket\n(define (f x) (match 'x ['x (f x)]))")
-              '((13 27) (27 41)))
+              (set '(13 27) '(27 41)))
 
 
 
@@ -189,35 +114,47 @@
                       actual?	 
                       phase-level)
   (list (list start-left start-right) (list end-left end-right)))
-(check-equal? (apply set (get-binding-arrows
-                          "#lang racket/base\n(require (only-in racket/base))"))
-              (set '((6 17) (19 26))     ;; to 'require'
-                   '((6 17) (28 35))))   ;; to 'only-in'
 
 (define-get-arrows get-binding-arrows/pxpy
-  (syncheck:add-arrow/name-dup/pxpy start-source-obj	 
-                                    start-left	 
+  (syncheck:add-arrow/name-dup/pxpy start-source-obj    
+                                    start-left  
                                     start-right
                                     start-px
                                     start-py
-                                    end-source-obj	 
-                                    end-left	 
+                                    end-source-obj      
+                                    end-left    
                                     end-right
                                     end-px
                                     end-py
-                                    actual?	 
+                                    actual?     
                                     phase-level
                                     require-arrows?
                                     name-dup?)
   (list (list start-left start-right start-px start-py)
         (list end-left end-right end-px end-py)))
-(check-equal? (apply set (get-binding-arrows/pxpy
-                          "#lang racket/base\n(require (only-in racket/base))"))
+
+(check-equal? (get-binding-arrows "#lang racket/base\n(require (only-in racket/base))")
+              (set '((6 17) (19 26))     ;; to 'require'
+                   '((6 17) (28 35))))   ;; to 'only-in'
+
+(check-equal? (get-binding-arrows
+               (string-append
+                "(module m racket/base\n"
+                "  (define x 4)\n"
+                "  x\n"
+                "  (let ([y 1]) y))\n"))
+              (set
+               '((10 21) (25 31))
+               '((10 21) (44 47))
+               '((32 33) (39 40))
+               '((50 51) (56 57))))
+
+(check-equal? (get-binding-arrows/pxpy
+               "#lang racket/base\n(require (only-in racket/base))")
               (set '((6 17 .5 .5) (19 26 .5 .5))     ;; to 'require'
                    '((6 17 .5 .5) (28 35 .5 .5))))
 
-(check-equal? (apply set (get-binding-arrows/pxpy
-                          "#lang racket\n(define/contract (f x) any/c f)"))
+(check-equal? (get-binding-arrows/pxpy "#lang racket\n(define/contract (f x) any/c f)")
               (set '((6 12 0.5 0.5) (14 29 0.5 0.5)) ;; point to define/contract
                    '((6 12 0.5 0.5) (36 41 0.5 0.5)) ;; point to any/c
                    '((31 32 0.5 0.5) (42 43 0.5 0.5))))  ;; from f to f
@@ -252,7 +189,7 @@
       "(def-a/boxed-a bar2 22)\n"
       "bar2-boxed\n")))
   (check-equal?
-   (for/set ([arrow (in-list all-arrows)]
+   (for/set ([arrow (in-set all-arrows)]
              #:when
              ;; make sure the arrow doesn't originate in the prefix
              ;; (not interested in testing those arrows)
@@ -268,43 +205,77 @@
            (subtract-prefix (list-ref arrow 1))))
    (set '((15 19 0.5 0.5) (24 28 0.5 0.5)))))
 
-;; Unused requires
-(let ()
-  (define-values (add-syntax done)
-    (make-traversal (make-base-namespace) #f))
 
-  (define collector%
-    (class (annotations-mixin object%)
-      (super-new)
-      (define unused-requires (set))
 
-      (define/override (syncheck:find-source-object stx) stx)
-      (define/override (syncheck:add-arrow start-text start-pos-left start-pos-right
-                                       end-text end-pos-left end-pos-right
-                                       actual? level)
-        (void))
+;                                                       
+;                                                       
+;                                                       
+;                                                       
+;                               ;;;                     
+;                                                       
+;  ;;; ;; ;;;;   ;; ;;; ;;; ;;; ;;; ;;; ;; ;;;;   ;;;;  
+;  ;;;;; ;; ;;; ;;;;;;; ;;; ;;; ;;; ;;;;; ;; ;;; ;;; ;; 
+;  ;;;  ;;; ;;; ;;; ;;; ;;; ;;; ;;; ;;;  ;;; ;;; ;;;    
+;  ;;;  ;;;;;;; ;;; ;;; ;;; ;;; ;;; ;;;  ;;;;;;;  ;;;;  
+;  ;;;  ;;;     ;;; ;;; ;;; ;;; ;;; ;;;  ;;;        ;;; 
+;  ;;;   ;;;;;; ;;;;;;; ;;;;;;; ;;; ;;;   ;;;;;; ;; ;;; 
+;  ;;;    ;;;;   ;; ;;;  ;; ;;; ;;; ;;;    ;;;;   ;;;;  
+;                   ;;;                                 
+;                   ;;;                                 
+;                                                       
+;                                                       
 
-      (define/override (syncheck:add-unused-require _ left right)
-          (set! unused-requires
-                (set-add unused-requires
-                         (list left right))))
-      (define/public (get-unused-requires) unused-requires)))
+(define-get-arrows get-unused-requires
+  (syncheck:add-unused-require _ left right)
+  (list left right))
 
-  (define annotations (new collector%))
-  (parameterize ([current-annotations annotations]
-                 [current-namespace (make-base-namespace)])
-    (add-syntax (expand
-                 (read-syntax
-                   'the-source
-                   (open-input-string
-                    (format "~s"
-                            `(module m racket/base
-                               (require racket/match)
-                               (+ 1 2)))))))
-    (done))
-  (check-equal?
-   (send annotations get-unused-requires)
-   (set '(31 43))))
+(check-equal? (get-unused-requires
+               "(module m racket/base (require racket/match) (+ 1 2)))")
+              (set '(31 43)))
+
+(define-get-arrows get-prefixed-require-reference
+  (syncheck:add-prefixed-require-reference req-src
+                                           req-pos-left
+                                           req-pos-right
+                                           prefix
+                                           prefix-src
+                                           prefix-left
+                                           prefix-right)
+  (list req-pos-left
+        req-pos-right
+        prefix
+        prefix-left
+        prefix-right))
+
+(check-equal?
+ (get-prefixed-require-reference
+  (string-append
+   "#lang racket/base\n"
+   "\n"
+   "(require (prefix-in : racket/string))\n"
+   "(:string-prefix? \"abcdefg\" \"abc\")\n"))
+ (set (list 41 54 ': 39 40)))
+
+
+
+;                                 
+;                                 
+;                                 
+;                                 
+;              ;;;                
+;                                 
+;  ;;; ;; ;;;  ;;;  ;;;;    ;;;   
+;  ;;;;;;;;;;; ;;; ;;; ;;  ;;;;;  
+;  ;;; ;;; ;;; ;;; ;;;    ;;;  ;; 
+;  ;;; ;;; ;;; ;;;  ;;;;  ;;;     
+;  ;;; ;;; ;;; ;;;    ;;; ;;;  ;; 
+;  ;;; ;;; ;;; ;;; ;; ;;;  ;;;;;  
+;  ;;; ;;; ;;; ;;;  ;;;;    ;;;   
+;                                 
+;                                 
+;                                 
+;                                 
+
 
 ;; make sure that the default arity of
 ;; syncheck:add-prefixed-require-reference
@@ -336,42 +307,8 @@
     (add-syntax (expand stx))
     (done)))
 
-(let ()
-  (define build-trace%
-    (class (annotations-mixin object%)
-      (define/override (syncheck:find-source-object stx)
-        (and (equal? src (syntax-source stx))
-             src))
-      (define/override (syncheck:add-prefixed-require-reference . args)
-        (set! args-seen (cons args args-seen)))
-      (super-new)))
-
-  (define args-seen '())
-
-  (define-values (in out) (make-pipe))
-  (thread
-   (λ ()
-     (displayln "#lang racket/base\n" out)
-     (writeln '(require (prefix-in : racket/string)) out)
-     (writeln '(:string-prefix? "abcdefg" "abc") out)
-     (close-output-port out)))
-  (define src "prefix.rkt")
-  (port-count-lines! in)
-  (define ns (make-base-namespace))
-  (define-values (add-syntax done)
-    (make-traversal ns (current-directory)))
-  (define prefixes-seen '())
-  (parameterize ([current-annotations (new build-trace%)]
-                 [current-namespace ns])
-    (define stx (with-module-reading-parameterization
-                  (λ () (read-syntax src in))))
-    (add-syntax (expand stx))
-    (done))
-
-  (check-equal? args-seen
-                 (list (list "prefix.rkt" 41 54
-                             ': "prefix.rkt" 39 40))))
-
+;; make sure that `make-traversal` is called with
+;; the containing directory by `show-content`
 (let ()
   (define tmp-dir (make-temporary-file "test-from-syncheck-direct-rkt~a" 'directory))
   (define src (build-path tmp-dir "prefix.rkt"))
@@ -419,3 +356,24 @@
       (check-pred file-exists? path)))
 
   (delete-directory/files tmp-dir))
+
+(check-not-exn
+ (λ ()
+   (define annotations
+     (new (class (annotations-mixin object%)
+            (super-new)
+            (define/override (syncheck:find-source-object stx)
+              stx))))
+
+   (define base-namespace (make-base-namespace))
+   (define-values (add-syntax done)
+     (make-traversal base-namespace #f))
+
+   (parameterize ([current-annotations annotations]
+                  [current-namespace base-namespace])
+     (eval '(require (for-syntax racket/base)))
+     (add-syntax
+      (expand
+       '(let-syntax ([m (λ (_) #`(let ([x 1]) x))])
+          (m))))
+     (done))))
