4b3743ce6bf645f7388fc39f25da08b83519e8bf
Robby Findler <robby@racket-lang.org>
2020-05-02 00:01:21 -0500

    fix a bug I introduced in c50934a62c

--- drracket/drracket/private/unit.rkt
+++ drracket/drracket/private/unit.rkt
@@ -2378,9 +2378,10 @@
           (toggle-show/hide-definitions)
           (update-shown)))
       (define/public (ensure-rep-shown rep)
-        (raise-argument-error 'ensure-rep-shown
-                              (format "~s" '(is-a?/c drracket:rep:text<%>))
-                              rep)
+        (unless (is-a? rep drracket:rep:text<%>)
+          (raise-argument-error 'ensure-rep-shown
+                                (format "~s" '(is-a?/c drracket:rep:text<%>))
+                                rep))
         (unless (eq? rep interactions-text)
           (let loop ([tabs tabs])
             (unless (null? tabs)

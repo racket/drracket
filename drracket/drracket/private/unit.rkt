racket/drracket

commit c50934a62c24d2806528b103226f3a39f5c772e5

Update unit.rkt and fix docs
closes #237

drracket/drracket/private/unit.rkt
@@ -2378,6 +2378,9 @@
           (toggle-show/hide-definitions)
           (update-shown)))
       (define/public (ensure-rep-shown rep)
+        (raise-argument-error 'ensure-rep-shown
+                              (format "~s" '(is-a?/c drracket:rep:text<%>))
+                              rep)
         (unless (eq? rep interactions-text)
           (let loop ([tabs tabs])
             (unless (null? tabs)

drracket/scribblings/tools/unit.scrbl
@@ -595,10 +595,12 @@ visible).
 
 }
 
-@defmethod[(ensure-rep-shown)
+@defmethod[(ensure-rep-shown [rep (is-a?/c drracket:rep:text<%>)])
            void?]{
 
-Shows the interactions window
+This method is called to force the rep window to be visible when, for
+example, an error message is put into the rep. Also ensures
+that the appropriate tab is visible, if necessary.
 
 
 }

commit 4b3743ce6bf645f7388fc39f25da08b83519e8bf

fix a bug I introduced in c50934a62c

drracket/drracket/private/unit.rkt
@@ -2378,9 +2378,10 @@
           (toggle-show/hide-definitions)
           (update-shown)))
       (define/public (ensure-rep-shown rep)
-        (raise-argument-error 'ensure-rep-shown
-                              (format "~s" '(is-a?/c drracket:rep:text<%>))
-                              rep)
+        (unless (is-a? rep drracket:rep:text<%>)
+          (raise-argument-error 'ensure-rep-shown
+                                (format "~s" '(is-a?/c drracket:rep:text<%>))
+                                rep))
         (unless (eq? rep interactions-text)
           (let loop ([tabs tabs])
             (unless (null? tabs)