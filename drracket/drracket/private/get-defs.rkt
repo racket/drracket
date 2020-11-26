racket/drracket

commit 988880f0cdb28433f309c8e4a43859024c035d5a

use the paren match to find the end of the identifier after the definition for the (define ...) popup menu
closes #436

drracket/drracket/private/get-defs.rkt
@@ -12,7 +12,8 @@
          (struct-out define-popup-info))
 
 ;; defn = (make-defn number string number number)
-(define-struct defn (indent name start-pos end-pos) #:mutable)
+(define-struct defn (indent name start-pos end-pos)
+  #:mutable #:transparent)
 
 (struct define-popup-info (prefix long-name short-name) #:transparent)
 
@@ -181,6 +182,9 @@
 (define (skip-past-non-whitespace text pos)
   (skip text (skip text pos char-whitespace?) (negate char-whitespace?)))
 
+(define (forward-to-next-token text pos)
+  (send text forward-match pos (send text last-position)))
+
 ;; get-defn-name : text number -> string
 ;; returns the name of the definition starting at `define-pos',
 ;; assuming that the bound name is the first symbol to appear
@@ -201,7 +205,64 @@
                              end-suffix-pos))))]
                 [else end-suffix-pos])]
          [start-name-pos (skip-whitespace/paren text end-header-pos)]
-         [end-name-pos (skip-to-whitespace/paren text start-name-pos)])
+         [end-name-pos (forward-to-next-token text start-name-pos)])
     (if (>= end-suffix-pos (send text last-position))
         (string-constant end-of-buffer-define)
         (send text get-text start-name-pos end-name-pos))))
+
+(module+ test
+  (require rackunit racket/gui/base framework)
+
+  (let ()
+    (define t (new racket:text%))
+    (send t insert "(define (f x) x)\n")
+    (send t insert "  (define (g x) x)\n")
+    (send t insert "(module m racket/base)\n")
+    (check-equal?
+     (get-definitions
+      (list (define-popup-info "(define" "(define ...)" "δ")
+            (define-popup-info "(module" "(module ...)" "ρ"))
+      #t
+      t)
+     (list (defn 0 "f" 0 18)
+           (defn 2 "  g" 19 35)
+           (defn 0 "m" 36 59)))
+    (check-equal?
+     (get-definitions
+      (list (define-popup-info "(module" "(module ...)" "ρ"))
+      #t
+      t)
+     (list (defn 0 "m" 36 59))))
+
+  (let ()
+    (define t (new racket:text%))
+    (send t insert "(define-metafunction L\n")
+    (send t insert "  M : any -> any\n")
+    (send t insert "  [(M any_1) any_1])\n")
+    (check-equal?
+     (get-definitions
+      (list (define-popup-info "(define" "(define ...)" "δ"))
+      #t
+      t)
+     (list (defn 0 "M" 0 61))))
+
+  (let ()
+    (define t (new racket:text%))
+    (send t insert "(define-metafunction L\n")
+    (send t insert "  [(M any_1) any_1])\n")
+    (check-equal?
+     (get-definitions
+      (list (define-popup-info "(define" "(define ...)" "δ"))
+      #t
+      t)
+     (list (defn 0 "M" 0 44))))
+
+  (let ()
+    (define t (new racket:text%))
+    (send t insert "(define (|(| x) x)\n")
+    (check-equal?
+     (get-definitions
+      (list (define-popup-info "(define" "(define ...)" "δ"))
+      #t
+      t)
+     (list (defn 0 "|(|" 0 19)))))

commit 109cad715b1ea5b99d6f77be2176af5ad5590de2

just skip any thing where we see a definition but cannot find the defined identifier
fixes a bug introduced in 988880f0cdb (before this it would not
raise an error but the code in 988880f0cdb raises an error)

drracket/drracket/private/get-defs.rkt
@@ -91,12 +91,17 @@
       (cond
         [(not defn-pos) null]
         [else
-         (let ([indent (get-defn-indent text defn-pos)]
-               [name (get-defn-name text (+ defn-pos tag-length))])
-           (set! min-indent (min indent min-indent))
-           (cons (make-defn indent name defn-pos defn-pos)
-                 (loop (+ defn-pos tag-length)
-                       new-find-state)))])))
+         (define indent (get-defn-indent text defn-pos))
+         (define name (get-defn-name text (+ defn-pos tag-length)))
+         (cond
+           [name
+            (set! min-indent (min indent min-indent))
+            (cons (make-defn indent name defn-pos defn-pos)
+                  (loop (+ defn-pos tag-length)
+                        new-find-state))]
+           [else
+             (loop (+ defn-pos tag-length)
+                        new-find-state)])])))
   
   ;; update end-pos's based on the start pos of the next defn
   (unless (null? defs)
@@ -192,23 +197,25 @@
 ;; usually finds the bound name, but it breaks for Redex
 ;; metafunction definitions (thus the hack below).
 (define (get-defn-name text define-pos)
-  (let* ([end-suffix-pos (skip-to-whitespace/paren text define-pos)]
-         [suffix (send text get-text define-pos end-suffix-pos)]
-         [end-header-pos 
-          (cond [(regexp-match #rx"^-metafunction(/extension)?$" suffix)
-                 => (λ (m)
-                      (let ([extension? (second m)])
-                        (skip-past-non-whitespace
-                         text
-                         (if extension?
-                             (skip-past-non-whitespace text end-suffix-pos)
-                             end-suffix-pos))))]
-                [else end-suffix-pos])]
-         [start-name-pos (skip-whitespace/paren text end-header-pos)]
-         [end-name-pos (forward-to-next-token text start-name-pos)])
-    (if (>= end-suffix-pos (send text last-position))
-        (string-constant end-of-buffer-define)
-        (send text get-text start-name-pos end-name-pos))))
+  (define end-suffix-pos (skip-to-whitespace/paren text define-pos))
+  (define suffix (send text get-text define-pos end-suffix-pos))
+  (define end-header-pos
+    (cond [(regexp-match #rx"^-metafunction(/extension)?$" suffix)
+           => (λ (m)
+                (let ([extension? (second m)])
+                  (skip-past-non-whitespace
+                   text
+                   (if extension?
+                       (skip-past-non-whitespace text end-suffix-pos)
+                       end-suffix-pos))))]
+          [else end-suffix-pos]))
+  (define start-name-pos (skip-whitespace/paren text end-header-pos))
+  (define end-name-pos (forward-to-next-token text start-name-pos))
+  (cond
+    [(not end-name-pos) #f]
+    [(>= end-suffix-pos (send text last-position))
+     (string-constant end-of-buffer-define)]
+    [else (send text get-text start-name-pos end-name-pos)]))
 
 ;; more tests are in tests/drracket/get-defs-test
 (module+ test
@@ -266,4 +273,15 @@
       (list (define-popup-info "(define" "(define ...)" "δ"))
       #t
       t)
-     (list (defn 0 "|(|" 0 19)))))
+     (list (defn 0 "|(|" 0 19))))
+
+  (let ()
+    (define t (new racket:text%))
+    (send t insert "(define)\n")
+    (check-equal?
+     (get-definitions
+      (list (define-popup-info "(define" "(define ...)" "δ"))
+      #t
+      t)
+     (list)))
+  )