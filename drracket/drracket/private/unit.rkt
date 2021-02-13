7f565911518ffe239ba896c8f3e11f7eec91595c
Robby Findler <robby@racket-lang.org>
2021-02-13 07:00:24 -0600

    use number-snip:remove-decimal-looking-number-snips-on-insertion-mixin
    related to racket/htdp#134

--- drracket/drracket/private/unit.rkt
+++ drracket/drracket/private/unit.rkt
@@ -550,7 +550,8 @@
                        (text:inline-overview-mixin
                         (text:all-string-snips-mixin
                          (text:ascii-art-enlarge-boxes-mixin
-                          text:info%))))))))))))))])
+                          (number-snip:remove-decimal-looking-number-snips-on-insertion-mixin
+                           text:info%)))))))))))))))])
        ((get-program-editor-mixin)
         (class* definitions-super% (drracket:unit:definitions-text<%>)
           (inherit is-locked? lock while-unlocked
