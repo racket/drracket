2e99b70e5e9049704b6b823a803b0a004ede0bcd
Robby Findler <robby@racket-lang.org>
2021-05-08 10:01:27 -0500

    adjust drracket's internal pict snip so that it exposes (possibly shared) bitmaps to the underlying wxme format in a way that it will only write the same ones once per file

--- drracket/drracket/private/pict-snip.rkt
+++ drracket/drracket/private/pict-snip.rkt
@@ -4,8 +4,10 @@
          racket/match
          racket/draw
          file/convertible
+         racket/format
          wxme
          (prefix-in r: racket/base))
+(module+ test (require rackunit))
 
 (provide pict-snip% snip-class reader)
 
@@ -38,11 +40,8 @@
     (define/override (copy) (new pict-snip% [w w] [h h] [d d] [a a] 
                                  [recorded-datum recorded-datum]))
     
-    (define/override (write f) 
-      (define prt (open-output-bytes))
-      (r:write (list w h d a recorded-datum) prt)
-      (define bytes (get-output-bytes prt))
-      (send f put (bytes-length bytes) bytes))
+    (define/override (write f)
+      (write-version-1-of-snip w h d a recorded-datum f))
 
     (define/override (find-scroll-step y)
       (inexact->exact (floor (/ y 12))))
@@ -89,10 +88,19 @@
 
 (define snip-class 
   (new (class snip-class%
+         (inherit reading-version set-version)
          (define/override (read f)
-           (parse-pict-snip-from-bytes
-            (send f get-unterminated-bytes)))
-         (super-new))))
+           (define version (reading-version f))
+           (case version
+             [(0)
+              (parse-version-0-pict-snip-from-bytes
+               (send f get-unterminated-bytes))]
+             [(1)
+              (parse-version-1-pict-snip
+               (λ () (send f get-unterminated-bytes))
+               (λ () (send f get-exact)))]))
+         (super-new)
+         (set-version 1))))
 
 (send snip-class set-classname (format "~s" (list '(lib "pict-snip.rkt" "drracket" "private")
                                                   '(lib "pict-snip.rkt" "drracket" "private"))))
@@ -102,22 +110,97 @@
   (new (class* object% (snip-reader<%>)
          (define/public (read-header version stream) (void))
          (define/public (read-snip text-only? version stream)
-           (define bytes (send stream read-raw-bytes 'pict-snip))
            (if text-only?
                #"#<pict-snip>"
-               (or (parse-pict-snip-from-bytes bytes) 
+               (or (case version
+                     [(0)
+                      (parse-version-0-pict-snip-from-bytes
+                       (send stream read-raw-bytes 'pict-snip))]
+                     [(1)
+                      (parse-version-1-pict-snip
+                       (λ () (send stream read-raw-bytes "drracket's pict-snip%"))
+                       (λ () (send stream read-integer "drracket's pict-snip%")))])
                    (error 'pict-snip.rkt "could not read pict-snip from stream"))))
          (super-new))))
 
 ;; parse-pict-snip-from-bytes : bytes -> (or/c (is-a?/c pict-snip%) #f)
-(define (parse-pict-snip-from-bytes bytes)
+(define (parse-version-0-pict-snip-from-bytes bytes)
   (let/ec escape
     (define prt (open-input-bytes bytes))
-    (define sexp (with-handlers ((exn:fail:read? (λ (x) (escape #f))))
+    (define sexp (with-handlers ([exn:fail:read? (λ (x) (escape #f))])
                    (read prt)))
     (match sexp
       [`(,(? real? w) ,(? real? h) ,(? real? d) ,(? real? a) ,recorded-datum)
-       (new pict-snip% [w w] [h h] [d d] [a a] 
+       (new pict-snip% [w w] [h h] [d d] [a a]
             [recorded-datum recorded-datum])]
       [else
        #f])))
+
+(define (parse-version-1-pict-snip get-some-bytes get-a-number)
+  (define prefix (bytes->string/utf-8 (get-some-bytes)))
+  (define w (get-a-number))
+  (define h (get-a-number))
+  (define d (get-a-number))
+  (define a (get-a-number))
+  (define byteses
+    (for/list ([_ (in-range (get-a-number))])
+      (get-some-bytes)))
+  (define rewriten-datum (read (open-input-bytes (get-some-bytes))))
+  (define recorded-datum
+    (let loop ([datum rewriten-datum])
+      (cond
+        [(pair? datum) (cons (loop (car datum)) (loop (cdr datum)))]
+        [(equal? datum prefix)
+         (begin0
+           (car byteses)
+           (set! byteses (cdr byteses)))]
+        [else datum])))
+  (new pict-snip% [w w] [h h] [d d] [a a]
+       [recorded-datum recorded-datum]))
+
+(define (write-version-1-of-snip w h d a recorded-datum f)
+  (define unique-string (get-unique-string recorded-datum))
+  (define unique-bytes (string->bytes/utf-8 unique-string))
+  (send f put unique-bytes)
+  (send f put w)
+  (send f put h)
+  (send f put d)
+  (send f put a)
+  (define-values (rewritten-datum byteses)
+    (rewrite-recorded-datum recorded-datum unique-string))
+  (send f put (length byteses))
+  (for ([bytes (in-list byteses)])
+    (send f put bytes))
+  (define bp (open-output-bytes))
+  (write rewritten-datum bp)
+  (send f put (get-output-bytes bp)))
+
+(define (rewrite-recorded-datum recorded-datum unique-string)
+  (define byteses '())
+  (define rewriten-datum
+    (let loop ([recorded-datum recorded-datum])
+      (match recorded-datum
+        [(cons a b) (cons (loop a) (loop b))]
+        [(? bytes?)
+         (set! byteses (cons recorded-datum byteses))
+         unique-string]
+        [else recorded-datum])))
+  (values rewriten-datum byteses))
+
+(define (get-unique-string recorded-datum)
+  (define prefix 0)
+  (let loop ([recorded-datum recorded-datum])
+    (cond
+      [(pair? recorded-datum)
+       (loop (car recorded-datum))
+       (loop (cdr recorded-datum))]
+      [(string? recorded-datum)
+       (define m (regexp-match? #rx"^bmpref([0-9]+):" recorded-datum))
+       (when m
+         (define n (string->number (list-ref m 1)))
+         (set! prefix (max (+ n 1) prefix)))]))
+  (~a "bmpref" prefix ":"))
+
+(module+ test
+  (check-equal? (get-unique-string '(((1) 2))) "bmpref0")
+  (check-equal? (get-unique-string '((("bmpref4") "bmpref1"))) "bmpref5"))
