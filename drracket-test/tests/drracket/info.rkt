5b9a5a442c375d95fc47267996e3a500db6ff588
Robby Findler <robby@racket-lang.org>
2024-08-20 10:08:41 -0500

    improve time-keystrokes.rkt
     - make the keystroke test run in drdr
     - fix a bug (need the window<%> guard)
     - rackety

--- drracket-test/tests/drracket/info.rkt
+++ drracket-test/tests/drracket/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
-(define tools '(("time-keystrokes.rkt")))
-(define tool-names '("Time Keystrokes"))
+(define drracket-tools '(("time-keystrokes.rkt")))
+(define drracket-tool-names '("Time Keystrokes"))
 (define compile-omit-paths '("snip" "image-and-comment-box.rkt"))
 
 (define test-timeouts '(("easter-egg.rkt" 300)
