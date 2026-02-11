f6f370e04e552fa07b53c477f6bc180faf512290
Robby Findler <robby@racket-lang.org>
2026-02-11 12:23:22 -0600

    fixes for errortrace-startup.rkt test

--- drracket-test/tests/drracket/errortrace-startup.rkt
+++ drracket-test/tests/drracket/errortrace-startup.rkt
@@ -1,8 +1,29 @@
 #lang racket/base
-(require "private/drracket-test-util.rkt")
+(require "private/drracket-test-util.rkt"
+         racket/gui/base
+         racket/class)
 
-(void (putenv "PLTDRDEBUG" "yes"))
+#|
 
-(fire-up-drracket-and-run-tests 
- (λ ()
-   (wait-for-drracket-frame)))
+this file cannot use anything except racket/gui/base
+and things that it depends on during the thunk passed
+to `fire-up-drracket-and-run-tests` or else errortrace
+seems to get confused. I'm not sure exactly why
+
+|#
+
+(void (putenv "PLTDRDEBUG" "yes")
+      (putenv "PLTNOTOOLS" "yes"))
+
+(parameterize ([current-namespace (make-gui-namespace)])
+  (fire-up-drracket-and-run-tests
+   (λ ()
+     (define (drracket-frame? frame)
+       (method-in-interface? 'get-execute-button (object-interface frame)))
+     (define (wait-for-drracket-frame-pred)
+       (define actives (get-top-level-windows))
+       (for/or ([active (in-list actives)])
+         (and active
+              (method-in-interface? 'get-execute-button (object-interface active))
+              active)))
+     (poll-until wait-for-drracket-frame-pred 400))))
